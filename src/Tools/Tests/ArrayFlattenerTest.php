8ef0bd2b266a38cfe3aaa244312bd4b0e3c91bb5
GijsPaulussen <gijs@tactics.be>
2018-10-24 14:34:47 +0200

    require-dev phpunit

---
+++ src/Tools/Tests/ArrayFlattenerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tools\Tests;
+
+use Common\Tests\TestCase;
+use Tools\ArrayFlattener;
+
+class ArrayFlattenerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function is_flattens()
+    {
+        $input = [
+            'foo' => 'bar',
+            'nested1' => [
+                'foo' => 'bar',
+                'nested2' => [
+                    'foo' => 'bar',
+                    'baz' => 'bazz',
+                ],
+            ],
+        ];
+
+        $expected = ['foo' => 'bar', 'nested1.foo' => 'bar', 'nested1.nested2.foo' => 'bar', 'nested1.nested2.baz' => 'bazz'];
+
+        $this->assertEquals($expected, ArrayFlattener::getFlat($input));
+    }
+
+    /**
+     * @test
+     */
+    public function can_return_flattened_key_value_string()
+    {
+        $input = [
+            'foo' => 'bar',
+            'nested1' => [
+                'foo' => 'bar',
+                'nested2' => [
+                    'foo' => 'bar',
+                    'baz' => 'bazz',
+                ],
+            ],
+        ];
+
+        $expected = 'foo=bar nested1.foo=bar nested1.nested2.foo=bar nested1.nested2.baz=bazz';
+
+        $this->assertEquals($expected, ArrayFlattener::getFlatKeyValueString($input));
+    }
+}
