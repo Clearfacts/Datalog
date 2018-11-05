078ab4ac8dbec4f06562f14a35364e447fc512f1
GijsPaulussen <gijs@tactics.be>
2018-11-05 15:25:31 +0100

    cli doenst have server params, this will prevent crashes in cli

--- src/Processor/SessionRequestProcessor.php
+++ src/Processor/SessionRequestProcessor.php
@@ -21,6 +21,8 @@ class SessionRequestProcessor
     public function processRecord(array $record)
     {
         if (null === $this->requestId) {
+            $this->requestId = substr(uniqid(), -8);
+
             if ('cli' === php_sapi_name()) {
                 $this->sessionId = getmypid();
             } else {
@@ -30,25 +32,30 @@ class SessionRequestProcessor
                 } catch (\RuntimeException $e) {
                     $this->sessionId = '????????';
                 }
+
+                $this->_server = [
+                    'http.url' => (@$_SERVER['HTTP_HOST']) . '/' . (@$_SERVER['REQUEST_URI']),
+                    'http.method' => @$_SERVER['REQUEST_METHOD'],
+                    'http.useragent' => @$_SERVER['HTTP_USER_AGENT'],
+                    'http.referer' => @$_SERVER['HTTP_REFERER'],
+                    'http.x_forwarded_for' => @$_SERVER['HTTP_X_FORWARDED_FOR'],
+                ];
+
+                $this->_post = $this->clean($_POST);
+                $this->_get = $this->clean($_GET);
             }
-            $this->requestId = substr(uniqid(), -8);
-            $this->_server = [
-                'http.url' => (@$_SERVER['HTTP_HOST']) . '/' . (@$_SERVER['REQUEST_URI']),
-                'http.method' => @$_SERVER['REQUEST_METHOD'],
-                'http.useragent' => @$_SERVER['HTTP_USER_AGENT'],
-                'http.referer' => @$_SERVER['HTTP_REFERER'],
-                'http.x_forwarded_for' => @$_SERVER['HTTP_X_FORWARDED_FOR'],
-            ];
-            $this->_post = $this->clean($_POST);
-            $this->_get = $this->clean($_GET);
         }
-        $record['http.request_id'] = $this->requestId;
-        $record['http.session_id'] = $this->sessionId;
-        $record['http.url'] = $this->_server['http.url'];
-        $record['http.method'] = $this->_server['http.method'];
-        $record['http.useragent'] = $this->_server['http.useragent'];
-        $record['http.referer'] = $this->_server['http.referer'];
-        $record['http.x_forwarded_for'] = $this->_server['http.x_forwarded_for'];
+
+        $record['request_id'] = $this->requestId;
+        $record['session_id'] = $this->sessionId;
+
+        if (!'cli' === php_sapi_name()) {
+            $record['http.url'] = $this->_server['http.url'];
+            $record['http.method'] = $this->_server['http.method'];
+            $record['http.useragent'] = $this->_server['http.useragent'];
+            $record['http.referer'] = $this->_server['http.referer'];
+            $record['http.x_forwarded_for'] = $this->_server['http.x_forwarded_for'];
+        }
 
         return $record;
     }
