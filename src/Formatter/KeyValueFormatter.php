e0f794d6dfbec47b24f0108325b8b1a705cd4d79
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2021-08-09 09:15:35 +0200

    Add return type to be compatible with sf5

--- src/Formatter/KeyValueFormatter.php
+++ src/Formatter/KeyValueFormatter.php
@@ -7,7 +7,7 @@ use Monolog\Formatter\JsonFormatter;
 
 class KeyValueFormatter extends JsonFormatter
 {
-    public function format(array $record)
+    public function format(array $record): string
     {
         return ArrayFlattener::getFlatKeyValueString(json_decode(parent::toJson($record), true));
     }
