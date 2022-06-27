45e74e2f01daef15256b3820f5713e5b4a2362b1
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2022-06-27 14:58:15 +0200

    [CLEARFACTS-6945] make KeyValueFormatter BC

--- src/Formatter/KeyValueFormatter.php
+++ src/Formatter/KeyValueFormatter.php
@@ -10,8 +10,12 @@ use Monolog\LogRecord;
 
 class KeyValueFormatter extends JsonFormatter
 {
-    public function format(LogRecord $record): string
+    public function format(LogRecord|array $record): string
     {
+        if ($record instanceof LogRecord) {
+            $record = $record->toArray();
+        }
+
         return ArrayFlattener::getFlatKeyValueString(json_decode($this->toJson($record), true, 512, JSON_THROW_ON_ERROR));
     }
 }
