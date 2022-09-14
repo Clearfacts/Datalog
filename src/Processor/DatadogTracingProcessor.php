0eeb0b301ec0dfa228591474e3814044eb1bad04
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2022-09-14 15:27:56 +0200

    [master] Check if tracing is enabled

--- src/Processor/DatadogTracingProcessor.php
+++ src/Processor/DatadogTracingProcessor.php
@@ -6,8 +6,12 @@ class DatadogTracingProcessor
 {
     public function processRecord(array $record)
     {
+        if (!extension_loaded('ddtrace')) {
+            return $record;
+        }
+
         $context = \DDTrace\current_context();
-        $record['dd'] .= [
+        $record['dd'] = [
             'trace_id' => $context['trace_id'],
             'span_id' => $context['span_id'],
         ];
